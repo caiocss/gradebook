5b4c5e769150eb8c2e7841a10e2c8792c1894d7c
Caio Santos <caio.santos687@gmail.com>
2019-07-23 16:09:00 -0300

    init project

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbersArray = new[] {10.1, 10.1 };
+            var numbersList = new List<double>() {10.1 , 10.1, 33.2, 40.2, 3.3};
+
+            double result = 0;
+
+            foreach (double number in numbersList)
+            {
+                result += number;
+            }
+            result = result / numbersList.Count;
+
+            Console.WriteLine($"The average result is: {result:N1}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello!");
+            }            
+        }
+    }
+}
